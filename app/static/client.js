204dc3cafc8d8fd61e70f4994a94d93e2db28c93
Chris PMaag <chrispmaag@gmail.com>
2019-03-15 19:47:02 -0700

    style: Update result format

--- app/static/client.js
+++ app/static/client.js
@@ -24,7 +24,7 @@ function analyze() {
     xhr.onload = function(e) {
         if (this.readyState === 4) {
             var response = JSON.parse(e.target.responseText);
-            el('result-label').innerHTML = `Result = ${response['result']}`;
+            el('result-label').innerHTML = `Score: ${response['result']}`;
         }
         el('analyze-button').innerHTML = 'Analyze';
     }
@@ -33,4 +33,3 @@ function analyze() {
     fileData.append('file', uploadFiles[0]);
     xhr.send(fileData);
 }
-
